ca8f19f79fc123f454379c7d65f6e9c7c4964187
Venkata Ramana Makka <129928303+venkat0x1@users.noreply.github.com>
2025-10-22 00:16:46 +0530

    Update LaneRatesDTO.java

--- src/main/java/com/xplor/read_excel/dto/LaneRatesDTO.java
+++ src/main/java/com/xplor/read_excel/dto/LaneRatesDTO.java
@@ -1,21 +1,21 @@
-package com.xplor.read_excel.dto;
+// package com.xplor.read_excel.dto;
 
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
-import com.xplor.read_excel.entity.CustomLaneRate;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+// import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+// import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+// import com.xplor.read_excel.entity.CustomLaneRate;
+// import lombok.AllArgsConstructor;
+// import lombok.Data;
+// import lombok.NoArgsConstructor;
 
-import java.util.List;
+// import java.util.List;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
-public class LaneRatesDTO {
+// @NoArgsConstructor
+// @AllArgsConstructor
+// @Data
+// public class LaneRatesDTO {
 
-    @JacksonXmlElementWrapper(useWrapping = false)
-    @JacksonXmlProperty(localName = "LaneRate")
-    List<CustomLaneRate> customLaneRates;
+//     @JacksonXmlElementWrapper(useWrapping = false)
+//     @JacksonXmlProperty(localName = "LaneRate")
+//     List<CustomLaneRate> customLaneRates;
 
-}
+// }
